925737b4d3b169786d7e5df7fe9a77c30e255b99
mimic <itmustbemimic@gmail.com>
2023-02-10 22:44:21 +0900

    add: read all users (test)

--- src/main/java/io/neond/genesis/controller/TestController.java
+++ src/main/java/io/neond/genesis/controller/TestController.java
@@ -1,10 +1,14 @@
 package io.neond.genesis.controller;
 
+import io.neond.genesis.domain.member.Member;
+import io.neond.genesis.domain.member.MemberRepository;
 import io.neond.genesis.service.MemberService;
 import io.neond.genesis.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,9 +16,15 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
     private final RoleService roleService;
+    private final MemberRepository memberRepository;
 
     @PostMapping("/addrole")
     public ResponseEntity<Long> saveRole(@RequestBody String roleName) {
         return ResponseEntity.ok(roleService.saveRole(roleName));
+    }
+
+    @GetMapping("/all")
+    public List<Member> findAll() {
+        return memberRepository.findAll();
     }
 }
