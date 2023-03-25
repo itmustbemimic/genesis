4182fe3fe120a809099fd1b8270f56c27d439494
mimic <itmustbemimic@gmail.com>
2023-03-25 22:35:27 +0900

    update: swagger 설명 추가

--- src/main/java/io/neond/genesis/domain/dto/request/UseTicketRequestDto.java
+++ src/main/java/io/neond/genesis/domain/dto/request/UseTicketRequestDto.java
@@ -1,6 +1,7 @@
 package io.neond.genesis.domain.dto.request;
 
 import io.neond.genesis.domain.entity.TicketHistory;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,9 +10,17 @@ import java.time.Instant;
 @Getter
 @AllArgsConstructor
 public class UseTicketRequestDto {
+
+    @Schema(example = "7a034705-7207-4791-9d0a-1e8371008b4b")
     private String uuid;
+
+    @Schema(example = "red")
     private String type;
+
+    @Schema(example = "음료수 사먹엇엉")
     private String usage;
+
+    @Schema(example = "2")
     private int amount;
 
     public TicketHistory toEntity(String _uuid){
