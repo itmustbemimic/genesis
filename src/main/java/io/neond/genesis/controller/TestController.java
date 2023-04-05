1f446649e63d3bd16975c2a542134847cf816cfe
mimic <itmustbemimic@gmail.com>
2023-04-06 01:36:34 +0900

    add: fcm test

--- src/main/java/io/neond/genesis/controller/TestController.java
+++ src/main/java/io/neond/genesis/controller/TestController.java
@@ -1,5 +1,9 @@
 package io.neond.genesis.controller;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import io.neond.genesis.domain.dto.request.AddMultipleTicketRequestDto;
 import io.neond.genesis.domain.dto.request.SingleTicketRequestDto;
 import io.neond.genesis.domain.dto.response.AdminMemberDto;
@@ -13,6 +17,8 @@ import io.neond.genesis.service.RoleService;
 import io.neond.genesis.service.TicketService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +35,7 @@ public class TestController {
     private final MemberRepository memberRepository;
     private final AdminService adminService;
     private final TicketService ticketService;
+    private final FirebaseMessaging firebaseMessaging;
 
     @PostMapping("/addrole")
     public ResponseEntity<Long> saveRole(@RequestBody String roleName) {
@@ -63,6 +70,35 @@ public class TestController {
     @GetMapping("/admins")
     public List<AdminMemberDto> getAdmins() {
         return adminService.getAdminMember();
+    }
+
+    @PostMapping("/fcmtest")
+    public ResponseEntity fcmTest(@RequestBody FcmTestDto fcmTestDto) {
+        Notification notification = Notification.builder()
+                .setTitle(fcmTestDto.title)
+                .setBody(fcmTestDto.body)
+                .setImage(fcmTestDto.image)
+                .build();
+
+        Message message = Message.builder()
+                .setToken(fcmTestDto.fcmToken)
+                .setNotification(notification)
+                .build();
+
+        try {
+            firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.ok(null);
+    }
+
+    public class FcmTestDto {
+        private String title;
+        private String body;
+        private String image;
+        private String fcmToken;
     }
 
 }
