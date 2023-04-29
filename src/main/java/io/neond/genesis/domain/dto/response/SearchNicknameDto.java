f123297a6f2a08c8693792ad403a3c573e369b15
mimic <itmustbemimic@gmail.com>
2023-04-29 18:31:43 +0900

    update: 유저닉네임 검색 안되던거 수정

--- src/main/java/io/neond/genesis/domain/dto/response/SearchNicknameDto.java
+++ src/main/java/io/neond/genesis/domain/dto/response/SearchNicknameDto.java
@@ -1,8 +1,10 @@
 package io.neond.genesis.domain.dto.response;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class SearchNicknameDto {
     private String nickname;
     private String uuid;
